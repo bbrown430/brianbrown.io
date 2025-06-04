f231c161fb6aa6b7a9a0b96d9b04c1d9a4a26017
Brian Brown <brianbrown2002@gmail.com>
2025-06-03 20:08:49 -0400

    logo links

--- src/components/landing-title.tsx
+++ src/components/landing-title.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { IconHero } from "./icon-hero";
+import LogoButton from "./ui/logo-button";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const LandingTitle: React.FC = () => {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center gap-4 ">
-      <div className="items-start max-w-[580px] p-4">
+    <div className="flex flex-col lg:flex-row items-center justify-center">
+      <div className="items-start max-w-[580px] p-4 gap-2 flex flex-col">
         <h1 className="text-8xl font-black">Brian Brown</h1>
         <h3 className="text-muted-foreground text-2xl">
           I’m a computer engineer based in Boston, MA. I’m passionate about
           creative solutions, automation, and innovation.
         </h3>
+        <div className="flex flex-row gap-4 mt-2">
+          <LogoButton
+            icon={FaLinkedin}
+            link="https://www.linkedin.com/in/brian-brown-neu/"
+          />
+          <LogoButton
+            icon={FaGithub}
+            link="https://github.com/bbrown430"
+          />
+        </div>
       </div>
       <IconHero />
     </div>
---
+++ src/components/ui/logo-button.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { IconType } from 'react-icons';
+
+interface LogoButtonProps {
+    icon: IconType;
+    link: string;
+}
+
+const LogoButton: React.FC<LogoButtonProps> = ({ icon: Icon, link }) => {
+    return (
+        <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center justify-center w-8 h-8 text-muted-foreground transition-colors ease-in-out duration-150 hover:text-flexoki-green-500"
+        >
+            <Icon className="w-12 h-12" />
+        </a>
+    );
+};
+
+export default LogoButton;
